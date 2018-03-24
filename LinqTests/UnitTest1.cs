f3840ab03619de5aa9698cf3ebbcc09ab8cd91b3
CoderEasonLin <lin07646@gmail.com>
2018-03-24 10:17:29 +0800

    new test case, use where.

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -2,6 +2,7 @@
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqTests
 {
@@ -40,6 +41,40 @@ namespace LinqTests
             };
 
             expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void linq_find_products_that_price_between_200_and_500()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.Where(x => x.Price >= 200 && x.Price <= 500);
+
+            var expected = new List<Product>()
+            {
+                new Product()
+                {
+                    Id = 2,
+                    Price = 210,
+                    Cost = 21,
+                    Supplier = "Yahoo"
+                },
+                new Product()
+                {
+                    Id = 3,
+                    Price = 310,
+                    Cost = 31,
+                    Supplier = "Odd-e"
+                },
+                new Product()
+                {
+                    Id = 4,
+                    Price = 410,
+                    Cost = 41,
+                    Supplier = "Odd-e"
+                }
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
     }
 }
